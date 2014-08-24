3f83edf4bc5fde5d33066f73914f89edcf534c0e
jlindsey15 <jwlsta15@aol.com>
2014-08-23 23:29:32 -0400

    changed header

--- client/main.js
+++ client/main.js
@@ -9,16 +9,11 @@ STATUS_PENDING=1;
 STATUS_APPROVED=2;
 STATUS_REJECTED=3;
 
-window.onscroll = function(){
-
-  if($(document).height()>=$(window).height()) {
-  {
-    Router.go('/top/40');
+window.onscroll = function() {
+  if($(document).height()>$(window).height()) {
+    Router.go('/top/40')
   }
-
-  
 }
-
 
 adminNav = adminNav.concat([
   {
